0d5bba343abf11c28e2e8c791c794284042d9540
felipebuso <felipe.buso@hotmail.com>
2025-03-27 09:42:26 -0300

    feat: set operacoes basica

--- src/edu/felipebuso/set/ConjuntoConvidados.java
+++ src/edu/felipebuso/set/ConjuntoConvidados.java
@@ -38,12 +38,18 @@ public class ConjuntoConvidados {
 
         System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
 
-//        conjuntoConvidados.adicionarConvidado("Alice", 1234);
-//        conjuntoConvidados.adicionarConvidado("Bob", 1235);
-//        conjuntoConvidados.adicionarConvidado("Charlie", 1235);
-//        conjuntoConvidados.adicionarConvidado("David", 1236);
-//
-//        System.out.println("Convidados no conjunto:");
-//        conjuntoConvidados.exibirConvidados();
+        conjuntoConvidados.adicionarConvidado("Alice", 1234);
+        conjuntoConvidados.adicionarConvidado("Bob", 1235);
+        conjuntoConvidados.adicionarConvidado("Charlie", 1235);
+        conjuntoConvidados.adicionarConvidado("David", 1236);
+
+        System.out.println("Convidados no conjunto:");
+        conjuntoConvidados.exibirConvidados();
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1236);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados após a remoção");
+
+        System.out.println("Convidados no conjunto após a remoção:");
+        conjuntoConvidados.exibirConvidados();
     }
 }
